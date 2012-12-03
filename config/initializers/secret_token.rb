9cb3022b208f9a908e376fd4b91adc2c41066097
Ekta Verma <eku4evr@gmail.com>
2012-12-04 02:49:05 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inmycity::Application.config.secret_token = '904dc18ab1a291e6fb85e2ba3e4964b5b8a537113ebd0166e62714bbf2cab0306d04dafd8b66f9d3d180cea91edd304cf906a569b524504e2640561712fe968a'
